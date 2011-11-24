9e64f6aa7b8a288f6d455a11474204581ebe9846
hideo55 <hide.o.j55@gmail.com>
2011-11-25 00:12:44 +0900

    initial commit

---
+++ lib/autolocale.pm
@@ -0,0 +1,32 @@
+package autolocale;
+use strict;
+use warnings;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=head1 NAME
+
+autolocale -
+
+=head1 SYNOPSIS
+
+  use autolocale;
+
+=head1 DESCRIPTION
+
+autolocale is
+
+=head1 AUTHOR
+
+Hideaki Ohno E<lt>hide.o.j55 {at} gmail.comE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
